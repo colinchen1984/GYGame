9b92ffcedde32fd6d646f76a0a8b6da2507649bf
beyond0924 <beyond0924@2bc07c12-943a-11dd-b351-877165d8dbbc>
2011-09-23 15:15:45 +0000

    1 修改了GatewayThread中在处理逻辑服务器发过来的数据包时可能导致分配的数据包没有还给包工厂 2 修改了服务器在处理缓存区的数据包时没有一次处理完需要处理的所有数据，导致延迟 3 修改了GYTableSerialization中的内存泄漏 4 修改了Win32Select中select后的循环判断条件，改进循环效率 5 添加用来测试服务器延迟的数据包

--- Bin/client.py
+++ Bin/client.py
@@ -3,26 +3,32 @@ import time
 import struct
 import random
 G_LoopCount = 1
-G_ClientCount = 1
-G_ServerAddress = ("127.0.0.1", 5555)
+G_ClientCount = 44
+G_ServerAddress = ("192.168.1.100", 5555)
 s = []
+gGUID = []
 for i in range(G_LoopCount):
 	for x in range(G_ClientCount):
 		t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
 			t.connect(G_ServerAddress)	
 			time.sleep(0.0001)
-			userID = random.randrange(12225545855, 1222554585500)
+			userID = random.randrange(100, 1222554585500)
+			gGUID.append(userID)
 			userName = "TestName"
 			testPacket = struct.pack("Q", userID)
 			testPacket += struct.pack("h", len(userName))
 			testPacket += struct.pack(("%ds" % len(userName)), userName)
+			testPacket += struct.pack("h", len(userName))
+			testPacket += struct.pack("h", len(userName))
+			testPacket += struct.pack("h", len(userName))
+			testPacket += struct.pack("h", len(userName))
 
 			packetID = 0
 			packetHead = struct.pack("hhc", 0, len(testPacket), '\0')
 
 			testPacket = packetHead + testPacket
-			s.append((testPacket, t))
+			s.append((testPacket, (t, i)))
 		except :
 			print x
 	time.sleep(0.1)
@@ -33,11 +39,17 @@ while(1):
 	back = []
 	for t in s:
 		begin = time.time()
-		t[1].send(t[0])
-		recvPacket = t[1].recv(len(t[0]))
+		print "Sending Packet"
+		t[1][0].send(t[0])
+		print "Recving Packet"
+		recvPacket = t[1][0].recv(len(t[0]) + 16)
 		back.append(t)
 		end = time.time() - begin
-		print "recv packet len %d, send packet len %d, count is %d spend %f" % (len(recvPacket), (len(t[0])) , count, end)
+		timeData = recvPacket[len(t[0]) - 8:]
+		
+		gatewayReceive, logicReceive, gatewayReceiveLogic = struct.unpack("QQQ", timeData)
+		# print "recv packet len %d, send packet len %d, count is %d spend %f" % (len(recvPacket), (len(t[0])) , count, end)
+		print "%u\t%d\t%d\t%d\n" % (gGUID[t[1][1]], logicReceive-gatewayReceive, gatewayReceiveLogic-logicReceive, gatewayReceiveLogic-gatewayReceive)
 		# except:
 			# print t[0]
 	s = back
